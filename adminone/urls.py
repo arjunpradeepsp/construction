177234afe1b46bdff4f064d5369a68b9e08aecf6
arjunpradeep <arjunpradeepsp@gmail.com>
2022-02-03 18:06:32 +0530

    indexedit

--- adminone/urls.py
+++ adminone/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('home/',views.fnHome),
+    path('home/',views.fnHome,name='home/'),
+    path('first/',views.fnFirst,name='first/')
 ]
--- adminone/views.py
+++ adminone/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def fnHome(request):
     return render(request,'home.html')
+
+
+
+
+
+
+
+def fnFirst(request0):
+       return render(request0,'welcome.html') 
+    
