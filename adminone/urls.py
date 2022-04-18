7284260b20525c0f2c2c3847763eae76ac306f18
arjunpradeep <arjunpradeepsp@gmail.com>
2022-04-18 10:33:07 +0530

    loginabout

--- adminone/urls.py
+++ adminone/urls.py
@@ -1,14 +1,14 @@
 from pydoc import visiblename
 from django.urls import path
-from . import views
+from .import views
 urlpatterns=[
     path('home',views.fnHome,name='home'),
-   
     path('login',views.fnLogin,name='login'),
-    path('path',views.fnabout,name='about'),
-    path('contact',views.fncontact,name='contact'),
+    path('plumb',views.fnPlumb,name='plumb'),
     path('offer',views.fnoffer,name='offer'),
     path('sign',views.fnsign,name='sign'),
-    path('path',views.fnmenu,name='menu'),
-    path('crt',views.fncrt,name='crt'),
+    path('electrc',views.fnElectrc,name='electrc'),
+    path('const',views.fnconst,name='const'),
+    path('paint',views.fnpaint,name='paint'),
+    path('homee',views.fnhomee,name='homee')
 ]
--- adminone/views.py
+++ adminone/views.py
@@ -1,29 +1,58 @@
-from cmath import log
-from urllib import request
 from django.shortcuts import render
 
 # Create your views here.
-def fnHome(request):
-    return render(request,'home.html')
-def fnFirst(request0):
-       return render(request0,'welcome.html') 
+def fnHome(request8):
+    return render(request8,'home.html')
+
     
 def fnLogin(request1):
     return render(request1,'login.html')
 
-def fnabout(request2):
-    return render(request2,'about.html')
-
-def fncontact(request3):
-    return render(request3,'contact.html')
-
-def fnmenu(request4):
-    return render(request4,'menu.html')
+def fnPlumb(request2):
+    return render(request2,'plumbing.html')
 
 def fnsign(request5):
     return render(request5,'sign.html')
 
 def fnoffer(request6):
     return render(request6,'offer.html')
-def fncrt(request7):
-    return render(request7,'crt.html')
+
+
+def fnElectrc(request7):
+    return render(request7,'electrc.html')
+
+def fnconst(request8):
+    return render(request8,'const.html')
+    
+def fnpaint(request9):
+    return render(request9,'paints.html') 
+
+def fnhomee(request10):
+    return render(request10,'homee.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
